4489867d4980d983973b30b9436e839dde13cc09
otmichael <ozmichaelt@gmail.com>
2017-12-29 21:18:19 -0800

    Test App

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import App from './App.js';
+import Downloads from './Downloads/Downloads.js';
+import Search from './Search/Search.js';
 import store from './store.js';
 
 describe('App', () => {
+
 	const app = mount (
 		<App />, {
 			context: { store }
-		});
+		}
+	);
+
 	it('always renders base-container div', () => {
 		const divs = app.find('.base-container');
 		expect(divs.length).toEqual(1);
 	});
+
 	describe('the base-container div', () => {
 		it('contains everything else that gets rendered', () => {
 			const divs = app.find('div');
 			const innerDivs = app.find('.base-container').children();
 			expect(innerDivs.length).toEqual(divs.length - 2);
 		});
+		it('always renders <Downloads />', () => {
+			expect(app.find(Downloads).length).toBe(1);
+		});
+		it('always renders <Search />', () => {
+			expect(app.find(Search).length).toBe(1);
+		});
 	});
+
 });
