d554a015909c8ee1e6c650f3c46590f5d2391aa5
otmichael <ozmichaelt@gmail.com>
2018-01-11 13:34:44 -0800

    Add modal perspective rotate animation and download button bounce-in animation
    - Fixes #42

--- src/VideoViewer/VideoViewer.js
+++ src/VideoViewer/VideoViewer.js
@@ -3,18 +3,16 @@ import { connect } from 'react-redux';
 import CircularProgressbar from 'react-circular-progressbar';
 import { setVideoViewer } from './VideoViewerActions.js';
 import { addDownload } from '../Downloads/DownloadActions.js';
+import { Transition } from 'react-transition-group';
+
+class AnimatedVideoViewer extends React.Component {
+
+}
 
 class VideoViewer extends React.Component {
 
-	getClasses(classList) { // a little verbose
-		if(this.props.showing) {
-			classList.push('showing');
-		}
-		return classList.join(' ');
-	}
-
 	close() {
-		this.props.setVideoViewer(false, {});
+		this.props.setVideoViewer(false, this.props.video);
 	}
 
 	download(mediaType) {
@@ -44,30 +42,47 @@ class VideoViewer extends React.Component {
 	}
 
 	render() {
-		if(this.props.video._id == undefined) {
-			return <div></div>;
-		}
+		let transitionStyles_container = {
+			entering: { visibility: 'visible', opacity: 1 },
+			entered: { visibility: 'visible', opacity: 1 },
+			exiting: { visibility: 'hidden', opacity: 0 },
+			exited: { visibility: 'hidden', opacity: 0 }
+		};
+		let transitionStyles_content = {
+			entering: { transform: 'rotateX(0deg)' },
+			entered: { transform: 'rotateX(0deg)' },
+			exiting: { transform: 'rotateX(-90deg)' },
+			exited: { transform: 'rotateX(-90deg)' }
+		};
 		let src = 'https://img.youtube.com/vi/' + this.props.video._id + '/hqdefault.jpg';
+		let shouldShow = this.props.video._id != undefined && this.props.showing;
 		return (
-			<div className={this.getClasses(['video-viewer'])}>
-				<div className={this.getClasses(['video-viewer-content'])}>
-					<div className='video-viewer-content-top' style={{'backgroundImage': `url(${src})`}}>
-						<div className='video-viewer-text-container'>
-							<h1 className='video-viewer-text'>{this.props.video.title}</h1>
-							<h3 className='video-viewer-text'>{this.props.video.uploader}</h3>
-							<h3 className='video-viewer-text'>{this.props.video.views}</h3>
-							<h3 className='video-viewer-text'>{this.props.video.duration}</h3>
-							<div className='download-buttons-container'>
-								{this.getButtonOrProgressBar(this.props.video, 'audio')}
-								{this.getButtonOrProgressBar(this.props.video, 'video')}
+			<Transition
+				in={shouldShow}
+				timeout={{enter: 0, exit: 300}}
+			>
+				{(state) => (
+					<div className='video-viewer' style={{...transitionStyles_container[state]}}>
+						<div className='video-viewer-content' style={{...transitionStyles_content[state]}}>
+							<div className='video-viewer-content-top' style={{'backgroundImage': `url(${src})`}}>
+								<div className='video-viewer-text-container'>
+									<h1 className='video-viewer-text'>{this.props.video.title}</h1>
+									<h3 className='video-viewer-text'>{this.props.video.uploader}</h3>
+									<h3 className='video-viewer-text'>{this.props.video.views}</h3>
+									<h3 className='video-viewer-text'>{this.props.video.duration}</h3>
+									<div className='download-buttons-container'>
+										{this.getButtonOrProgressBar(this.props.video, 'audio')}
+										{this.getButtonOrProgressBar(this.props.video, 'video')}
+									</div>
+								</div>
+							</div>
+							<div className='close-viewer' onClick={this.close.bind(this)}>
+								<i className="fa fa-times"></i>
 							</div>
 						</div>
 					</div>
-					<div className='close-viewer' onClick={this.close.bind(this)}>
-						<i className="fa fa-times"></i>
-					</div>
-				</div>
-			</div>
+				)}
+			</Transition>
 		);
 	}
 }
