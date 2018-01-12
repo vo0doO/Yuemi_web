448b3f4736ea4d3c54bded4b23c3ae7a6914590c
otmichael <ozmichaelt@gmail.com>
2018-01-11 18:19:22 -0800

    Add mobile and apple detection

---
+++ src/lib/isMobile.js
@@ -0,0 +1,28 @@
+const is_mobile = () => { 
+	if( navigator.userAgent.match(/Android/i)
+		|| navigator.userAgent.match(/webOS/i)
+		|| navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+		|| navigator.userAgent.match(/BlackBerry/i)
+		|| navigator.userAgent.match(/Windows Phone/i)
+	){
+		return true;
+	}
+	else {
+		return false;
+	}
+};
+
+const is_apple_mobile = () => {
+	if( navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+	){
+		return true;
+	} else {
+		return false;
+	}
+};
+
+export { is_mobile, is_apple_mobile };
